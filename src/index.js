46a8236d5a43e92b3821b7f6e006bb3f7c9a5e8b
Sagar Rathna Sabapathy <33907389+sabapathy-sagar@users.noreply.github.com>
2019-03-30 20:57:21 +0100

    Note app setup

--- src/index.js
+++ src/index.js
@@ -3,28 +3,54 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function App({ initCount }) {
-  const [count, setCount] = useState(initCount || 0);
-  const [text, setText] = useState("");
+const NoteApp = () => {
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState("");
 
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
-  const reset = () => setCount(initCount);
-  const updateText = e => setText(e.target.value);
+  const updateTitle = e => setTitle(e.target.value);
+  const addNote = e => {
+    e.preventDefault();
+    setNotes([...notes, { title }]);
+    //clear the tilte after adding it to the notes
+    setTitle("");
+  };
 
   return (
-    <div className="App">
-      <h1>Bonjour!</h1>
-      <p>
-        {text}: {count}
-      </p>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>-1</button>
-      <button onClick={reset}>RESET</button>
-      <input value={text} onChange={updateText} />
+    <div>
+      <h2>Notes</h2>
+      {notes.map(note => (
+        <div key={note.title}>{note.title}</div>
+      ))}
+      <form onSubmit={addNote}>
+        <input value={title} onChange={updateTitle} />
+        <button>Add Note</button>
+      </form>
     </div>
   );
-}
+};
+
+// function App({ initCount }) {
+//   const [count, setCount] = useState(initCount || 0);
+//   const [text, setText] = useState("");
+
+//   const increment = () => setCount(count + 1);
+//   const decrement = () => setCount(count - 1);
+//   const reset = () => setCount(initCount);
+//   const updateText = e => setText(e.target.value);
+
+//   return (
+//     <div className="App">
+//       <h1>Bonjour!</h1>
+//       <p>
+//         {text}: {count}
+//       </p>
+//       <button onClick={increment}>+1</button>
+//       <button onClick={decrement}>-1</button>
+//       <button onClick={reset}>RESET</button>
+//       <input value={text} onChange={updateText} />
+//     </div>
+//   );
+// }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App initCount={5} />, rootElement);
+ReactDOM.render(<NoteApp />, rootElement);

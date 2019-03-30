ed999ef429ef8b9681584933cdf2c097b39e3321
Sagar Rathna Sabapathy <33907389+sabapathy-sagar@users.noreply.github.com>
2019-03-30 20:28:38 +0100

    text state

--- src/index.js
+++ src/index.js
@@ -5,19 +5,23 @@ import "./styles.css";
 
 function App({ initCount }) {
   const [count, setCount] = useState(initCount || 0);
+  const [text, setText] = useState("");
 
   const increment = () => setCount(count + 1);
-
   const decrement = () => setCount(count - 1);
   const reset = () => setCount(initCount);
+  const updateText = e => setText(e.target.value);
 
   return (
     <div className="App">
       <h1>Bonjour!</h1>
-      <p>Count: {count}</p>
+      <p>
+        {text}: {count}
+      </p>
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
       <button onClick={reset}>RESET</button>
+      <input value={text} onChange={updateText} />
     </div>
   );
 }

e310856e7d6678d4402f6f5a9a662b241fef15f0
Sagar Rathna Sabapathy <33907389+sabapathy-sagar@users.noreply.github.com>
2019-03-31 11:49:48 +0200

    useEffect to read/write notes to localstorage

--- src/index.js
+++ src/index.js
@@ -3,65 +3,70 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-// const NoteApp = () => {
-//   const [notes, setNotes] = useState([]);
-//   const [title, setTitle] = useState("");
+const NoteApp = () => {
+  const notesData = JSON.parse(localStorage.getItem("notes"));
+  const [notes, setNotes] = useState(notesData || []);
+  const [title, setTitle] = useState("");
 
-//   const updateTitle = e => setTitle(e.target.value);
-//   const addNote = e => {
-//     e.preventDefault();
-//     setNotes([...notes, { title }]);
-//     //clear the tilte after adding it to the notes
-//     setTitle("");
-//   };
+  const updateTitle = e => setTitle(e.target.value);
+  const addNote = e => {
+    e.preventDefault();
+    setNotes([...notes, { title }]);
+    //clear the tilte after adding it to the notes
+    setTitle("");
+  };
 
-//   const removeNote = title =>
-//     setNotes(notes.filter(note => note.title !== title));
-
-//   return (
-//     <div>
-//       <h2>Notes</h2>
-//       {notes.map(note => (
-//         <div key={note.title}>
-//           <h3>{note.title}</h3>
-//           <button onClick={() => removeNote(note.title)}>X</button>
-//         </div>
-//       ))}
-//       <form onSubmit={addNote}>
-//         <input value={title} onChange={updateTitle} />
-//         <button>Add Note</button>
-//       </form>
-//     </div>
-//   );
-// };
-
-function App({ initCount }) {
-  const [count, setCount] = useState(initCount || 0);
-  const [text, setText] = useState("");
-
+  //store the notes in localStorage
   useEffect(() => {
-    console.log("use effect");
-    document.title = count;
+    localStorage.setItem("notes", JSON.stringify(notes));
   });
-
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
-  const reset = () => setCount(initCount || 0);
-  const updateText = e => setText(e.target.value);
+  const removeNote = title =>
+    setNotes(notes.filter(note => note.title !== title));
 
   return (
-    <div className="App">
-      <h1>Bonjour!</h1>
-      <p>
-        {text}: {count}
-      </p>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>-1</button>
-      <button onClick={reset}>RESET</button>
-      <input value={text} onChange={updateText} />
+    <div>
+      <h2>Notes</h2>
+      {notes.map(note => (
+        <div key={note.title}>
+          <h3>{note.title}</h3>
+          <button onClick={() => removeNote(note.title)}>X</button>
+        </div>
+      ))}
+      <form onSubmit={addNote}>
+        <input value={title} onChange={updateTitle} />
+        <button>Add Note</button>
+      </form>
     </div>
   );
-}
+};
+
+// function App({ initCount }) {
+//   const [count, setCount] = useState(initCount || 0);
+//   const [text, setText] = useState("");
+
+//   useEffect(() => {
+//     console.log("use effect");
+//     document.title = count;
+//   });
+
+//   const increment = () => setCount(count + 1);
+//   const decrement = () => setCount(count - 1);
+//   const reset = () => setCount(initCount || 0);
+//   const updateText = e => setText(e.target.value);
+
+//   return (
+//     <div className="App">
+//       <h1>Bonjour!</h1>
+//       <p>
+//         {text}: {count}
+//       </p>
+//       <button onClick={increment}>+1</button>
+//       <button onClick={decrement}>-1</button>
+//       <button onClick={reset}>RESET</button>
+//       <input value={text} onChange={updateText} />
+//     </div>
+//   );
+// }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App initCount={5} />, rootElement);
+ReactDOM.render(<NoteApp />, rootElement);

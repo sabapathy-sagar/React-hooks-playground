7ca09926609b95ce652a3ed2ddafa67cfee76867
Sagar Rathna Sabapathy <33907389+sabapathy-sagar@users.noreply.github.com>
2019-03-31 19:37:56 +0200

    custom hooks to track mouse movement

---
+++ src/hooks/useMousePosition.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+const useMousePosition = () => {
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0
+  });
+
+  const handleMouseMove = e => {
+    setPosition({
+      x: e.pageX,
+      y: e.pageY
+    });
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousemove", handleMouseMove);
+
+    // clean up event listener
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return position;
+};
+
+export { useMousePosition as default };
